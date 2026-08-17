7f8e8e839377adcb8519deb9e6947432948c42ee
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2026-08-17 15:53:19 +0530

    Added jwt functionality on api gateway side

---
+++ gateway/api-gateway/src/main/java/com/laughingenigma/api_gateway/config/JwtSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.laughingenigma.api_gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+@Configuration
+public class JwtSecurityConfig {
+    @Value("${jwt.public-key}")
+    private String publicKey;
+
+    @Bean
+    public JwtDecoder jwtDecoder() throws Exception {
+
+        byte[] keyBytes = Base64.getDecoder().decode(publicKey);
+
+        X509EncodedKeySpec keySpec =
+                new X509EncodedKeySpec(keyBytes);
+
+        KeyFactory keyFactory =
+                KeyFactory.getInstance("RSA");
+
+        PublicKey key = keyFactory.generatePublic(keySpec);
+
+        return NimbusJwtDecoder.withPublicKey((RSAPublicKey) key)
+                .build();
+    }
+}
---
+++ gateway/api-gateway/src/main/java/com/laughingenigma/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.laughingenigma.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity http) {
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
+                )
+                .build();
+    }
+}
