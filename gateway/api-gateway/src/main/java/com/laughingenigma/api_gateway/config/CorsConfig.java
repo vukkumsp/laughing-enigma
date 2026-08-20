83e77718e00692d749886af5fd18da3077af47a7
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2026-08-20 09:01:21 +0530

    added CORS allowed url in env for deployment ease

--- gateway/api-gateway/src/main/java/com/laughingenigma/api_gateway/config/CorsConfig.java
+++ gateway/api-gateway/src/main/java/com/laughingenigma/api_gateway/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.laughingenigma.api_gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -11,13 +12,16 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
+    @Value("${visual.enigma}")
+    private String visualEnigma;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(
-                List.of("http://localhost:4200")
+                List.of(visualEnigma)
         );
 
         configuration.setAllowedMethods(
