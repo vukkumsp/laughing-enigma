491b9d1e79ed73e085ab0f2d31b0a24b2fbd056d
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2026-08-20 12:26:27 +0530

    customer-service will return valid value along with user datails

--- services/customer-service/src/main/java/com/laughingenigma/customer_service/dto/Profile.java
+++ services/customer-service/src/main/java/com/laughingenigma/customer_service/dto/Profile.java
@@ -4,12 +4,19 @@ public class Profile {
     public String username;
     public String firstname;
     public String lastname;
+    public boolean valid;
+    public Profile(String username, String firstname, String lastname, boolean valid) {
+        this.username = username;
+        this.valid = valid;
+    }
 
     public Profile() {
+        this.valid = false;
     }
     public Profile(String username, String firstname, String lastname) {
         this.username = username;
         this.firstname = firstname;
         this.lastname = lastname;
+        this.valid = true;
     }
 }
