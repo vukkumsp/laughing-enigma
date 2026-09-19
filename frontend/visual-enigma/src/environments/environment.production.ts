6dffb351e3329b4ff67ffe5c1a94c7ae7de7c170
V. Sai Prakash <9028439+vukkumsp@users.noreply.github.com>
2026-09-19 18:35:16 +0530

    added /api/v1 to frontend base url as well

--- frontend/visual-enigma/src/environments/environment.production.ts
+++ frontend/visual-enigma/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-gateway-egdv.onrender.com'
+  apiUrl: 'https://api-gateway-egdv.onrender.com/api/v1'
 };
--- frontend/visual-enigma/src/environments/environment.ts
+++ frontend/visual-enigma/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080/api/v1'
   // apiUrl: 'https://api-gateway-egdv.onrender.com'
 };
